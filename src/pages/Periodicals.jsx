4107fc7ef40f35c7f23377f62478d5aa7ab980f2
Kirn Kim <squrkidev@gmail.com>
2024-03-29 16:54:39 -0700

    Assignment completed

--- src/pages/Periodicals.jsx
+++ src/pages/Periodicals.jsx
@@ -1,54 +1,94 @@
-import React from 'react'
+import { React, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useEffect, useState } from "react"
+import "./styles.css"
 
 export default function Books() {
     const navigate = useNavigate()
-    const periodicals = {
-        'a': {
+    const [newItem, setNewItem] = useState({ title: "", author: "", imgURL: "", impact: "", })
+    const [periodicals, setPeriodicals] = useState([
+        {
             title: "Bear's Guide to Earning Non-Traditional College Degrees",
             author: "John Bear, PhD",
             imgURL: "https://m.media-amazon.com/images/I/91hzJNovnOL._SY466_.jpg",
             impact: "Gave me ideas to help me focus on my education while on 'sabbatical', ultimately earning my bachelor's degree while still inside"
         },
-        'b': {
+        {
             title: "Wired",
             author: "Condé Nast",
             imgURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJBUgt1mLDOd8IU3pcm76bdOspYVRo4a3DPOek2IRwbA&s",
             impact: "Kept me informed on current social issues that involved technology"
         },
-        'c': {
+        {
             title: "Popular Science",
             author: "recurrent",
             imgURL: "https://www.popsci.com/uploads/2023/03/21/PopSci_LogoStacked_Black_s.png",
             impact: "Taught me about current technology and future possibilities"
         },
-        'd': {
+        {
             title: "USA Today",
             author: "Gannett",
             imgURL: "https://yt3.googleusercontent.com/ytc/AIdro_mNVdvBGR50BTpC2dQ7PsSyUxJVzic9j2dWXmRU0ME=s900-c-k-c0x00ffffff-no-rj",
             impact: "Kept me informed about current events and issues"
         },
-        'e': {
+        {
             title: "Maximum PC",
             author: "Future US",
             imgURL: "https://upload.wikimedia.org/wikipedia/en/thumb/f/fa/Maximum_PC_Holiday_2018_cover.jpg/220px-Maximum_PC_Holiday_2018_cover.jpg",
             impact: "Kept me informed about computer technology and how computer enthusiasts maximized capabilities"
         },
+    ])
+
+    const addPeriodical = (newItem) => {
+        setPeriodicals([...periodicals, newItem])
+    }
+
+    function handleSubmit(newItem) {
+        if (newItem.title === "") return
+        addPeriodical(newItem)
+        setNewItem({ title: "", author: "", imgURL: "", impact: "", })
+        document.getElementById("newTitle").value = ""
+        document.getElementById("newAuthor").value = ""
+        document.getElementById("newImgURL").value = ""
+        document.getElementById("newImpact").value = ""
     }
 
     return (
         <>
             <h1>Periodicals I Read Inside</h1>
             <div className='Periodicals'>
-                {Object.keys(periodicals).map((key) => (
-                    <table>
-                        <img src={periodicals[key].imgURL} width="100" />
-                        <tr><td>Title</td><td>:</td><td>{periodicals[key].title}</td></tr>
-                        <tr><td>Author/Publisher</td><td>:</td><td>{periodicals[key].author}</td></tr>
-                        <tr><td>Impact</td><td>:</td><td>{periodicals[key].impact}</td></tr>
-                    </table>
+                {periodicals.map((val) => (
+                    < div className='row' key={val.title}>
+                        <div className='column left'>
+                            <img src={val.imgURL} height="75" />
+                        </div>
+                        <div className='column right'>
+                            <table><tbody >
+                                <tr key={val.title}><td>Title</td><td>:</td><td>{val.title}</td></tr>
+                                <tr key={val.author}><td>Author/Publisher</td><td>:</td><td>{val.author}</td></tr>
+                                <tr key={val.impact}><td>Impact</td><td>:</td><td>{val.impact}</td></tr>
+                            </tbody></table>
+                        </div>
+                    </div>
                 ))}
+            </div>
+            <h2>Suggestions to add?</h2>
+            <div onSubmit={e => e.target.value = ""}>
+                <p>Book Title: <input id='newTitle'
+                    onChange={e => setNewItem({ ...newItem, "title": e.target.value })}
+                    type="text" /></p>
+                <p>Author/Publisher: <input id='newAuthor'
+                    onChange={e => setNewItem({ ...newItem, "author": e.target.value })}
+                    type="text" /></p>
+                <p>Image URL: <input id='newImgURL'
+                    onChange={e => setNewItem({ ...newItem, "imgURL": e.target.value })}
+                    type="text" /></p>
+                <p>Impact: <input id='newImpact'
+                    onChange={e => setNewItem({ ...newItem, "impact": e.target.value })}
+                    type="text" /></p>
+                <button onClick={() => {
+                    handleSubmit(newItem)
+                }
+                } className="btn">Add</button>
             </div>
             <button onClick={() => navigate('../Home')}>Go Home</button>
         </>
--- src/pages/Websites.jsx
+++ src/pages/Websites.jsx
@@ -1,41 +1,83 @@
-import React from 'react'
+import { React, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import "./styles.css"
 
 export default function Websites() {
     const navigate = useNavigate()
-
-    const websites = {
-        'a': {
+    const [newItem, setNewItem] = useState({ name: "", URL: "", imgURL: "", impact: "", })
+    const [websites, setWebsites] = useState([
+        {
             name: "YouTube",
             URL: "www.youtube.com",
             imgURL: "https://lh3.googleusercontent.com/3zkP2SYe7yYoKKe47bsNe44yTgb4Ukh__rBbwXwgkjNRe4PykGG409ozBxzxkrubV7zHKjfxq6y9ShogWtMBMPyB3jiNps91LoNH8A=s500",
             impact: "General entertainment, news, and education/instruction"
         },
-        'b': {
-            title: "Reddit",
+        {
+            name: "Reddit",
             URL: "www.reddit.com",
             imgURL: "https://www.redditinc.com/assets/images/site/Reddit_Icon_FullColor-1_2023-11-29-161416_munx.jpg",
             impact: "General entertainment, information, and education/instruction"
         },
-        'c': {
-            title: "Netflix",
+        {
+            name: "Netflix",
             URL: "www.netflix.com",
             imgURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdx7CHtJT1-gtx_-KiZr0ud_wBsivDKr--WHT2WOsgGg&s",
             impact: "General entertainment and escapism"
         },
+    ])
+
+    const addWebsite = (newItem) => {
+        setWebsites([...websites, newItem])
     }
+
+
+    function handleSubmit(newItem) {
+        if (newItem.name === "") return
+        addWebsite(newItem)
+        setNewItem({ name: "", URL: "", imgURL: "", impact: "", })
+        document.getElementById("newName").value = ""
+        document.getElementById("newURL").value = ""
+        document.getElementById("newImgURL").value = ""
+        document.getElementById("newImpact").value = ""
+    }
+
     return (
         <>
             <h1>Websites</h1>
             <div className='Websites'>
-                {Object.keys(websites).map((key) => (
-                    <table>
-                        <img src={websites[key].imgURL} width="100" />
-                        <tr><td>Title</td><td>:</td><td>{websites[key].title}</td></tr>
-                        <tr><td>URL</td><td>:</td><td>{websites[key].author}</td></tr>
-                        <tr><td>Impact</td><td>:</td><td>{websites[key].impact}</td></tr>
-                    </table>
+                {websites.map((val) => (
+                    < div className='row' key={val.name}>
+                        <div className='column left'>
+                            <img src={val.imgURL} height="75" />
+                        </div>
+                        <div className='column right' key='keyval'>
+                            <table><tbody>
+                                <tr><td>Website</td><td>:</td><td>{val.name}</td></tr>
+                                <tr><td>URL</td><td>:</td><td>{val.URL}</td></tr>
+                                <tr><td>Impact</td><td>:</td><td>{val.impact}</td></tr>
+                            </tbody></table>
+                        </div>
+                    </div>
                 ))}
+            </div>
+            <h2>Suggestions to add?</h2>
+            <div onSubmit={e => e.target.value = ""}>
+                <p>Website name: <input id='newName'
+                    onChange={e => setNewItem({ ...newItem, "name": e.target.value })}
+                    type="text" /></p>
+                <p>URL: <input id='newURL'
+                    onChange={e => setNewItem({ ...newItem, "URL": e.target.value })}
+                    type="text" /></p>
+                <p>Image URL: <input id='newImgURL'
+                    onChange={e => setNewItem({ ...newItem, "imgURL": e.target.value })}
+                    type="text" /></p>
+                <p>Impact: <input id='newImpact'
+                    onChange={e => setNewItem({ ...newItem, "impact": e.target.value })}
+                    type="text" /></p>
+                <button onClick={() => {
+                    handleSubmit(newItem)
+                }
+                } className="btn">Add</button>
             </div>
             <button onClick={() => navigate('../Home')}>Go Home</button>
         </>
